bed9d69de496483c7314137a82ca6aa4fb275862
kkamara <itchykilljoy@protonmail.com>
2018-09-26 14:30:13 +0100

    added migrations

---
+++ database/migrations/2018_09_26_132004_create_episodes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('podcasts_id')->references('id')->on('podcasts');
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('download_path')->unique();
+            $table->string('description');
+            $table->string('episode_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
